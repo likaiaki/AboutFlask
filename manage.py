419a9b57a05816a9c7ca231083d29b0a135ac74d
yudashan <likaikai316@163.com>
2020-09-28 20:27:41 -0700

    加载静态文件渲染首页

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@ import os
 from flask_migrate import MigrateCommand
 from flask_script import Manager
 from App import create_app, models
+from flask import render_template
+
 
 env = os.environ.get("FLASK_ENV", "development")
 app = create_app(env)
@@ -12,7 +14,7 @@ manager.add_command('db', MigrateCommand)
 
 @app.route("/")
 def index():
-    return "index"
+    return render_template("news/index.html")
 
 if __name__ == '__main__':
     manager.run()
